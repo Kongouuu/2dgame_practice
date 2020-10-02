7722e38b6cb65afbf5d3b483193ff58c7d9ad676
Kongouuu <2301788091@qq.com>
2020-10-02 17:08:10 +0800

    Set up game loop

---
+++ src/Game.cpp
@@ -0,0 +1,118 @@
+#include "./Game.h"
+
+#include <iostream>
+
+#include "./Constants.h"
+
+// Constructor
+Game::Game() {
+    isRunning = false;
+}
+
+// Destructor
+Game::~Game() {
+}
+
+// Dummy variable
+float projectilePosX = 0.0f;
+float projectilePosY = 0.0f;
+float projectileVelX = 1.0f;
+float projectileVelY = 1.0f;
+
+// Getter of isRunning state
+bool Game::IsRunning() const {
+    return isRunning;
+}
+
+// Initializer
+void Game::Initialize(int width, int height) {
+    // SDL_Init initializes the library
+    // If not zero, there is error
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "Error initializing SDL." << std::endl;
+        return;
+    }
+
+    // *** Create Window ***
+    window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED,  // x position of window
+        SDL_WINDOWPOS_CENTERED,  // y position of window
+        width,
+        height,
+        SDL_WINDOW_BORDERLESS);
+    if (!window) {
+        std::cerr << "Error creating SDL window." << std::endl;
+        return;
+    }
+
+    // *** Create Renderer ***
+    // Second option is about driver, -1 is default
+    // Third option is about flag, set to nothing by 0
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer) {
+        std::cerr << "Error creating SDL renderer." << std::endl;
+        return;
+    }
+
+    // If no error, set running to true
+    isRunning = true;
+    return;
+};
+
+// Input processor
+void Game::ProcessInput() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch (event.type) {
+        // Clicking the X on the window
+        case SDL_QUIT: {
+            isRunning = false;
+            break;
+        }
+        // If keydown
+        case SDL_KEYDOWN: {
+            // If keydown is escape
+            if (event.key.keysym.sym == SDLK_ESCAPE) {
+                isRunning = false;
+            }
+        }
+        default: {
+            break;
+        }
+    }
+};
+
+void Game::Update() {
+    projectilePosX += projectileVelX;
+    projectilePosY += projectileVelY;
+}
+
+void Game::Render() {
+    // Set up rgb and alpha
+    SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
+    // Clear back buffer [SDL has double buffer]
+    SDL_RenderClear(renderer);
+
+    // Struct a rectangle
+    SDL_Rect projectile {
+        (int)projectilePosX,
+        (int)projectilePosY,
+        10,
+        10
+    };
+    
+    // *** Draw the rectangle ***
+    // Set color of rectangle
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255 ,255);
+    SDL_RenderFillRect(renderer, &projectile);
+
+    SDL_RenderPresent(renderer);
+};
+
+void Game::Destroy() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+};
---
+++ src/Game.h
@@ -0,0 +1,24 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+class Game {
+    private:
+        bool isRunning;
+        SDL_Window *window;     // window of game
+        SDL_Renderer *renderer; // renderer of game
+
+    public:
+        Game(); //Constructor
+        ~Game(); //Destructor
+        bool IsRunning() const;
+        void Initialize(int width, int height);
+        void ProcessInput();
+        void Update();
+        void Render();
+        void Destroy();
+};
+#endif
